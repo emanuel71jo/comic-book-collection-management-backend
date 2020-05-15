82ad214aafbcce95d4db5ea75eb1cadfc896ebba
emanuel71jo <emanuel71jo@gmail.com>
2020-05-15 09:12:08 -0300

    finished setup initial

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const router = require('./routes');
 
+const app = express();
+
+app.use(cors);
+app.use(express.json);
 app.use(router);
 
 app.use((req, res, next)=>{
@@ -19,4 +23,4 @@ app.use((error, req, res, next)=>{
     });
 })
 
-app.listen(3000);
+app.listen(3333);
