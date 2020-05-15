fb4a7dafb3fc7e070cbb0375b0e61d89026fc13c
emanuel71jo <emanuel71jo@gmail.com>
2020-05-15 10:31:21 -0300

    final setup of the comic routes

--- src/controllers/ComicController.js
+++ src/controllers/ComicController.js
@@ -5,6 +5,11 @@ module.exports = {
             message: "list all of the comic"
         })
     },
+    async store ( req, res ) {
+        return res.json({
+            message: "list one of options"
+        })
+    },
     async create ( req, res ) {
         return res.json({
             message: "creted a new comic"
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 const ComicController = require('./controllers/ComicController');
 
-router.get("/comicbook", ComicController.index);
+router
+    .get("/comicbook", ComicController.index)
+    .get("/comicbook/options", ComicController.store)
+    .post("/comicbook", ComicController.create)
+    .delete("/comicbook", ComicController.delete)
+    .put("/comicbook", ComicController.update)
 
 
 module.exports = router;
