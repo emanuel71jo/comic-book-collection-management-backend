0c79fddc75cbf0f0f749306ebaa52c869b7c9e9c
emanuel71jo <emanuel71jo@gmail.com>
2020-05-15 08:59:04 -0300

    Treatment = should not found

--- src/app.js
+++ src/app.js
@@ -4,4 +4,19 @@ const router = require('./routes');
 
 app.use(router);
 
+app.use((req, res, next)=>{
+    const error = new Error("Could not found");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    return res.send({
+       error: {
+           message: error.message
+       } 
+    });
+})
+
 app.listen(3000);
