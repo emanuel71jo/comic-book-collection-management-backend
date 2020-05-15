2b6a00b998a58d93a4ed9eaed16d8838c1ba7090
emanuel71jo <emanuel71jo@gmail.com>
2020-05-15 15:50:10 -0300

    set location of migration from knex

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,10 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './src/database/db.sqlite3'
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`
     }
   }
 
