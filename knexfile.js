88944deb0869d13d0d2ae15125a4a757ee79838c
emanuel71jo <emanuel71jo@gmail.com>
2020-05-15 15:45:12 -0300

    configuration from knex

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
+// Update with your config settings.
+
 module.exports = {
-    development: {
-      client: 'sqlite3',
-      connection: {
-        filename: './src/database/db.sqlite'
-      },
-      migrations: {
-        directory: './src/database/migrations'
-      },
-      useNullAsDefault: true,
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite3'
     }
-}
+  }
+
+};
