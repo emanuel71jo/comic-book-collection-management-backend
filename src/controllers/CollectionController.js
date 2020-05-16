8f365fe75874e940b5564f3c3ff5d7933979dcde
emanuel71jo <emanuel71jo@gmail.com>
2020-05-16 09:41:14 -0300

    created a new collection, methon create

--- src/controllers/CollectionController.js
+++ src/controllers/CollectionController.js
@@ -6,6 +6,21 @@ module.exports = {
 
         return res.json(results);
     },
+    async create ( req, res, next ) {
+
+        try {
+            
+            const { name, publishing_company} = req.body;
+
+            await knex('collection').insert({ name, publishing_company });
+
+            return res.status(201).send();
+
+        } catch (error) {
+            next(error);
+        }
+
+    },
     async delete ( req, res ) {
         return res.json({
             message: "removed a colletion"
