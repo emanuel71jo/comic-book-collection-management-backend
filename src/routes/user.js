cb080167cbb8d2070ec9881b277c14ca46f6004d
emanuel71jo <emanuel71jo@gmail.com>
2020-05-21 09:20:12 -0300

    import from user controller

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express();
 
+const UserController = require('../controllers/UserController');
+
 router
     .get("/user", UserController.index)
     .post("/user", UserController.create)
