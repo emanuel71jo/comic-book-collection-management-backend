16b67e1b1d452b03bc3a6a401cad7dc48f2dc79b
emanuel71jo <emanuel71jo@gmail.com>
2020-05-26 09:37:31 -0300

    replan results for return in collection

--- src/controllers/CollectionController.js
+++ src/controllers/CollectionController.js
@@ -4,8 +4,39 @@ module.exports = {
     async index ( req, res, next ) {
         try {
 
-            const results = await knex("collection");
+            const collections = await knex('collection');
+            const comics = await knex('comic');
 
+            const results = {
+                length: collections.length,
+                collections: collections.map(
+                    (collection) => {
+
+                        const collection_comics = comics.filter(
+                            (comic) => {
+                                return comic.collection_id === collection.id;
+                            }
+                        )
+
+                        return {
+                            collection_id: collection.id,
+                            collection_name: collection.name,
+                            publishing_company: collection.publishing_company,
+                            length_comics: collection_comics.length,
+                            comics: collection_comics.map(
+                                (collection_comic) => {
+                                    return {
+                                        title: collection_comic.title,
+                                        status: collection_comic.status,
+                                        file: collection_comic.file
+                                    }
+                                }
+                            )
+                        };
+                    }
+                )
+            }
+                
             return res.json(results);
 
         } catch (error) {
