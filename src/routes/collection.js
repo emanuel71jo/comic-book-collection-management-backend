3904ee2bb7e751219a5bf6cb9e7ed059c3e63b3b
emanuel71jo <emanuel71jo@gmail.com>
2020-05-26 09:38:48 -0300

    add middleware authorization in router index at collection

--- src/routes/collection.js
+++ src/routes/collection.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express();
 
 const CollectionController = require('../controllers/CollectionController');
+const AutenticationMiddleware = require('../middleware/autentication');
+
 
 router
-    .get("/collection", CollectionController.index)
+    .get("/collection", AutenticationMiddleware, CollectionController.index)
     .delete("/collection/:id", CollectionController.delete);
 
 module.exports = router;
