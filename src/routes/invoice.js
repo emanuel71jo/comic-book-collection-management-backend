4abeda81f85191c4dd9c41573b1ebc5052fda0d0
emanuel71jo <emanuel71jo@gmail.com>
2020-05-27 09:10:26 -0300

    add middleware admin in router get from invoice

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -3,7 +3,9 @@ const router = express();
 
 const InvoiceController = require('../controllers/InvoiceController');
 
+const AdminMiddleware = require('../middleware/admin');
+
 router
-    .get("/invoice", InvoiceController.index);
+    .get("/invoice", AdminMiddleware, InvoiceController.index);
 
 module.exports = router;
