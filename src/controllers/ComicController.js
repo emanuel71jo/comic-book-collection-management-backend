ccc0a9b9a96d7bfc853ba92d7fff89cb5b25d358
emanuel71jo <emanuel71jo@gmail.com>
2020-05-27 08:35:46 -0300

    comic only can removed if it is not reading or loaning

--- src/controllers/ComicController.js
+++ src/controllers/ComicController.js
@@ -86,12 +86,42 @@ module.exports = {
         try {
             const { id } = req.params;
 
+            let results;
+
+            const comic_is_reading = await knex('reading')
+                .whereNull('evaluation')
+                .where('comic_id', id)
+                .count('*');
+            
+            if(comic_is_reading[0]['count(*)'] !== 0){
+                results = {
+                    message: 'Not permission for remove a comic because this comic is reading'
+                }
+                return res.status(202).send(results);
+            }
+
+            const comic_id_loan = await knex('loan')
+                .whereNull('date_devolution')
+                .where('comic_id', id)
+                .count('*');
+
+            if(comic_id_loan[0]['count(*)'] !== 0){
+                results = {
+                    message: 'Not permission for remove a comic because this comic is loaning'
+                }
+                return res.status(202).send(results);
+            }
+            
             await knex("comic")
                 .where({ id })
                 .where("status", 1)
                 .del();
             
-            return res.send();
+            results = {
+                message: 'Comic removed with success'
+            }
+            
+            return res.send(results);
 
         } catch (error) {
             next(error);
